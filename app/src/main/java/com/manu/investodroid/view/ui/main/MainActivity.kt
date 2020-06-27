49cecd6a6a2a7f7cd19aad81e1a8816147fbd75f
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-06-27 17:43:24 -0400

    Initial app setup

---
+++ app/src/main/java/com/manu/investodroid/view/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.manu.investodroid.view.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manu.investodroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
