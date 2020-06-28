811f4d3afcc035c07cad40cc7eedfe6ee0faa4a1
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-06-27 21:15:36 -0400

    Created the UI to display stocks using a recycler view. Added provision to use navigation component and bottom navigation in the project

--- app/src/main/java/com/manu/investodroid/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/manu/investodroid/view/ui/main/MainActivity.kt
@@ -2,11 +2,27 @@ package com.manu.investodroid.view.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.manu.investodroid.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        //set toolbar text
+        findViewById<Toolbar>(R.id.toolbar).title = "INVESTODROID"
+
+        //set bottom navigation view
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
+
+
     }
 }
